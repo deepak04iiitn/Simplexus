2ffe8cd21410e1cf9b0c2ad87691916495a6616e
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-11-22 01:43:39 +0530

    created the basic structure of the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <HelmetProvider>
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <div className="flex-grow">
+              <Routes>
+                <Route path='/' element={<Home />} />
+                {/* <Route path='/about' element={<About />} />
+                <Route path='/sign-in' element={<SignIn />} />
+                <Route path='/sign-up' element={<SignUp />} /> */}
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+    </HelmetProvider>
+  )
+}
